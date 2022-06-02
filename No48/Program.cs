e54a22133874b396ce7f52942530d4005cdabda3
Katherine_20 <mob1911494@gmail.com>
2022-06-02 13:03:07 +0300

    random array Amn = m+n

---
+++ No48/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+int GetNumber (string msg)
+{
+    Console.WriteLine(msg);
+    string ValueFromConsole = Console.ReadLine();
+    if (int.TryParse(ValueFromConsole, out int num))
+    {
+        return num;
+    }
+    else 
+    {
+        Console.WriteLine("Вы ввели не число, попробуйте еще раз!");
+    }
+    return num;
+}
+
+void FillArray(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i+j;
+        }
+    }
+}
+
+void PrintArray(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string message1 = "Введите количество строк: ";
+string message2 = "Введите количество рядов: ";
+int m = GetNumber(message1);
+int n = GetNumber(message2);
+
+int [,] matrix = new int [m,n];
+FillArray(matrix);
+PrintArray(matrix);
