2a62d9472093439100d39cb47b6800a1f3d3aaa6
Katherine_20 <mob1911494@gmail.com>
2022-06-02 12:38:38 +0300

    random arraz

--- No46/Program.cs
+++ No46/Program.cs
@@ -4,6 +4,21 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
+int GetNumber (string msg)
+{
+    Console.WriteLine(msg);
+    string ValueFromConsole = Console.ReadLine();
+    if (int.TryParse(ValueFromConsole, out int num))
+    {
+        return num;
+    }
+    else 
+    {
+        Console.WriteLine("Вы ввели не число, попробуйте еще раз!");
+    }
+    return num;
+}
+
 void PrintArray(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++) // function.GetLength(0/1) - узнаем длину массива 
@@ -22,11 +37,17 @@ void FillArray(int[,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(1,10); // [1;10)
+            matr[i,j] = new Random().Next(); 
         }
     }
 }
-int[,] matrix = new int[3, 4];
+
+string message1 = "Введите количество строк: ";
+string message2 = "Введите количество рядов: ";
+int m = GetNumber(message1);
+int n = GetNumber(message2);
+
+int[,] matrix = new int[m, n];
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
